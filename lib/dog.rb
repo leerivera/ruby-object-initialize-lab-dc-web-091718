2c8af38a55a2d103ef30acaa043389ec058e72b5
Nick Rivera <leerivera80@gmail.com>
2018-09-24 03:59:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog 
-  def initialize(name, breed="Mutt")
+  def initialize(name, breed="Mutt")#breed = "mutt" if no input given this returns "mutt"
     @name = name
     @breed = breed
   end
